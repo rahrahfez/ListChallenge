04cce264e9a5627ae0e6fc7850d0b4a2fd4881c5
David <rahrahfez@hotmail.com>
2019-10-28 12:29:24 -0400

    moved folders around for dockerfile

---
+++ ListChallengeApi/ListChallengeServer/Contracts/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expressions);
+        IQueryable<T> FindAll();
+        void Create(T entity);
+        void DeleteAll(IEnumerable<T> entity);
+        void Delete(T entity);
+    }
+}
