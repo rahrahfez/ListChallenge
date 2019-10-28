9f46f1b8d9488c9065e6a884bfce61b0a618ed93
David <rahrahfez@hotmail.com>
2019-10-28 15:36:04 -0400

    connecting to db

--- ListChallengeApi/ListChallengeServer/Program.cs
+++ ListChallengeApi/ListChallengeServer/Program.cs
@@ -3,22 +3,43 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Entities;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ListChallengeServer
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .MigrateDatabase<RepositoryContext>()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        public static IWebHost MigrateDatabase<T>(this IWebHost webHost) where T : DbContext
+        {
+            var serviceScopeFactory = (IServiceScopeFactory)webHost
+                .Services.GetService(typeof(IServiceScopeFactory));
+
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var dbContext = services.GetRequiredService<T>();
+                dbContext.Database.Migrate();
+            }
+            return webHost;
+        }
     }
 }
